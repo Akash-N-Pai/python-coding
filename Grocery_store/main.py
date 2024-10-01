d6cfa5a580cda2ea223979c0387098a4c2a55172
Akash Pai <akashvn.pai@gmail.com>
2024-09-30 20:42:02 -0500

    item change function added

--- Grocery_store/main.py
+++ Grocery_store/main.py
@@ -13,6 +13,21 @@ class Grocery_Store_checkout:
         self.coustomer_line[customer_id] = line_number
         self.coustomer_items[customer_id] = number_Of_items
 
+    def item_change(self,coustomer_id,new_item_num)
+        line_number = self.coustomer_line[coustomer_id]
+        if new_item_num > self.coustomer_items[coustomer_id]:
+            self.lines[line_number].remove[coustomer_id]
+            self.lines[line_number].apend[coustomer_id]
+        elif  new_item_num ==0:
+            self.lines[line_number].remove[coustomer_id]
+            del self.coustomer_line[coustomer_id]
+            del self.coustomer_items[coustomer_id]
+            self.Customer_exit(coustomer_id)
+        else:
+            self.coustomer_items[coustomer_id] = new_item_num
         
+
+
+
 
 
